c697f62d33deb6acfe54b3f2d95194de896fa4f3
Piero <piero@prehnit.hr>
2021-01-18 23:19:59 +0100

    * cleanup + added useful stuff to README

--- test/public/145.c
+++ test/public/145.c
@@ -1,6 +1,11 @@
 /*
 to COMPILE run:
 `emcc 145.c -Os -s WASM=1 -s SIDE_MODULE=1 -o test.wasm`
+https://github.com/emscripten-core/emscripten/wiki/WebAssembly-Standalone
+https://gist.github.com/kripken/59c67556dc03bb6d57052fedef1e61ab#file-hello_world-html-L24
+
+to activate `emcc` cmd on your machine go and see your .bashrc file and search for emsdk
+or go to folder emsdk in /home/${user}/bin and run `source /home/piero/bin/emsdk/emsdk_env.sh` 
 */
 
 int all_odd(int n) {
